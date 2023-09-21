5d024a60725744526557d9f4c8c8f1827d96f149
a92340a <a92340a@gmail.com>
2023-09-21 14:32:52 +0800

    apiserver: replace SocketIO mechanism and dockerize design

---
+++ server_producer/my_logger.py
@@ -0,0 +1,37 @@
+import os
+import sys
+import logging
+from datetime import datetime
+import time
+
+
+# datetime
+now = datetime.now()
+today = now.strftime('%Y%m%d')
+current_time = now.strftime('%H%M%S')
+
+
+class MyLogger(logging.Logger):
+    def __init__(self, name, level=logging.INFO):
+        super().__init__(name, level)
+        self.logger_formatter = logging.Formatter(u'[%(asctime)s][%(levelname)s]: %(message)s [%(threadName)s]', datefmt='%Y-%m-%d %H:%M:%S')
+
+    def console_handler(self):
+        console_handler = logging.StreamHandler(sys.stdout)
+        console_handler.setFormatter(self.logger_formatter)
+        self.addHandler(console_handler)
+
+    def file_handler(self, day):
+        log_path = f'log/{self.name}_{day}.log'
+        os.makedirs(os.path.dirname(log_path), exist_ok=True)
+        file_handler = logging.FileHandler(log_path)
+        file_handler.setFormatter(self.logger_formatter)
+        self.addHandler(file_handler)
+
+
+
+if __name__ == '__main__':
+    test_logger = MyLogger(__name__)
+    test_logger.file_handler(today)
+    test_logger.info(current_time)
+    test_logger.info(time.time())
