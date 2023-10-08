8d22620c0ad31dbc93988e68559085903e367cce
a92340a <a92340a@gmail.com>
2023-10-08 19:56:22 +0800

    feat($ compile): add spider dynamic websites of banks(from money101)

---
+++ credit_card_scraper/spiders/ctbc.py
@@ -0,0 +1,59 @@
+import time
+import random
+from datetime import datetime
+import scrapy
+from credit_card_scraper.items import CreditCardScraperItem
+
+# datetime
+now = datetime.now()
+today_date = now.date()
+today = now.strftime('%Y-%m-%d')
+
+class CtbcSpider(scrapy.Spider):
+    name = "ctbc"
+    allowed_domains = ["https://www.money101.com.tw"]
+    start_urls = ["https://www.money101.com.tw/%E4%BF%A1%E7%94%A8%E5%8D%A1/%E5%85%A8%E9%83%A8"]
+    handle_httpstatus_list = [302] 
+    custom_settings = {
+        'ITEM_PIPELINES': {
+            "credit_card_scraper.pipelines.CreditCardScraperPipeline": 300,
+        }
+    }
+
+
+    def parse(self, response):
+        time.sleep(random.randint(3, 5))
+        url = 'https://www.money101.com.tw/%E4%BF%A1%E7%94%A8%E5%8D%A1/%E5%85%A8%E9%83%A8?providers=%E4%B8%AD%E5%9C%8B%E4%BF%A1%E8%A8%97'
+        yield scrapy.Request(url, callback=self.parse_ctbc, dont_filter=True)
+
+
+    def parse_ctbc(self,response):
+        boxes = response.css('article.block.rounded.bg-white.shadow')
+        for box in boxes:
+            source = '中信'
+            bank_name = '中信, 中國信託銀行, 肥牡蠣, 牡蠣, 822, ctbc, we are family'
+            card_image = box.css('div.flex.flex-col.p-2 div div div div div a img::attr(src)').get()
+            if not card_image:
+                card_image = box.css('div.flex.flex-col.p-2 div div a img::attr(src)').get()
+            card_name = box.css('div.flex.items-center.justify-between a div h2::text').get()
+            content = box.css('div.flex.flex-col.p-2 div div div div div.flex-1 ::text').getall()
+            card_content = ''.join(content).replace('回饋','回饋,').replace('上限','上限,')
+            if not card_content:
+                content = box.css('div.flex.flex-col.p-2 div div.flex-1 ::text').getall()
+                card_content = ''.join(content).replace('回饋','回饋,').replace('上限','上限,')
+            card_link = 'https://www.ctbcbank.com/twrbo/zh_tw/cc_index/cc_product/cc_introduction_index.html'
+            create_dt = today
+            create_timestamp = int(time.time())
+            
+            item = CreditCardScraperItem()
+            item['source'] = source
+            item['bank_name'] = bank_name
+            item['card_image'] = card_image
+            item['card_name'] = card_name
+            item['card_content'] = card_content
+            item['card_link'] = card_link
+            item['create_dt'] = create_dt
+            item['create_timestamp'] = create_timestamp
+            yield item
+         
+
