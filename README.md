5b2f6503ce925d0e5ab073ff8f739b22e7eb50d4
a92340a <a92340a@gmail.com>
2023-10-11 11:55:11 +0800

    refactor the files and folders for scrapeops

--- README.md
+++
@@ -1,2 +0,0 @@
-# Credit_All_In_One
-AppWorks School personal project
--- application.py
+++
@@ -1,9 +0,0 @@
-import os
-from dotenv import load_dotenv
-from server_producer import app, socketio
-
-load_dotenv()
-
-if __name__ == '__main__':
-    socketio.run(app, host=os.getenv('COMMON_HOST'), port=os.getenv('PRODUCER_PORT'), debug=True, allow_unsafe_werkzeug=True)
-
