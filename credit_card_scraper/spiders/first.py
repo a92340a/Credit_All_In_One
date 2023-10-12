f3993235460c373f9b315c5f1a4071158228a3b1
a92340a <a92340a@gmail.com>
2023-10-12 16:33:16 +0800

    feat($ compile): adjust spiders of first, kaohsiung, taichung

--- credit_card_scraper/spiders/first.py
+++ credit_card_scraper/spiders/first.py
@@ -27,7 +27,7 @@ class FirstSpider(scrapy.Spider):
         boxes = response.css("div.card-single")
         for box in boxes:
             source = '一銀'
-            bank_name = '一銀, 第一, 第一銀行, 龐德, 007, first bank'
+            bank_name = '一銀, 第一銀行, 龐德, 007, first bank'
             card_image = '/static/images/first_bank/' + box.css('div.card-single-face div.card-single-img::attr(style)').get().split("'")[1].split('/sites/card/image/')[1].replace('?verifyType=Type1','') + '.jpg'
             card_name = box.css('div.card-single-features strong::text').get()
             
