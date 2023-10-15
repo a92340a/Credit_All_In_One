d8af0bd6e22718d51b30de674205e1314cc974a7
a92340a <a92340a@gmail.com>
2023-10-15 10:48:18 +0800

    style($ browser): add a collapse effect with showing first 2 line in the beginning

--- server_producer/static/dashboard_scripts.js
+++ server_producer/static/dashboard_scripts.js
@@ -29,3 +29,18 @@ function toggleCard() {
 //   <textarea class="form-control" placeholder="Leave a comment here" id="floatingTextarea"></textarea>
 //   <label for="floatingTextarea">Comments</label>
 // </div>
+
+// read more collapse
+document.querySelectorAll('.read-more').forEach(function(item) {
+  item.addEventListener('click', function(event) {
+      event.preventDefault(); // Prevent the default behavior of the link
+      var qaAnswer = this.parentElement.querySelector('.qa-answer-box').querySelector('.qa-answer');
+      qaAnswer.classList.toggle('expanded');
+      this.classList.toggle('collapsed');
+      if (this.classList.contains('collapsed')) {
+          this.innerHTML = '- Read Less';
+      } else {
+          this.innerHTML = '+ Read More';
+      }
+  });
+});
